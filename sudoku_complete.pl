5df8cc9ad5ed5a9a706b1f09302865877164fd5e
JulieWouters <Julie.wouters@student.kuleuven.be>
2015-05-04 22:25:22 +0200

    nog wa dingen geprobeerd!

--- sudoku_complete.pl
+++ sudoku_complete.pl
@@ -11,16 +11,17 @@ convert(Board,GoodBoard) :-
     ),
     array_list(GoodBoard,GoodList).
 
-solve(BoardList,Vboard,Rboard,Cboard) :-
+solve(Options,BoardList,B) :-
     convert(BoardList,Board),
-    sudoku(Board,Vboard,Rboard,Cboard), print_board(Board), writeln(" "), print_board(Vboard), writeln(" "), print_board(Rboard), writeln(" "), print_board(Cboard).
+    sudoku(Board,Vboard,Rboard,Cboard,B, Options), 
+    print_board(Board), writeln(" "), print_board(Vboard).
 
-sudoku(Board,Vboard,Rboard,Cboard) :-
-    constraints(Board,Vboard,Rboard,Cboard),
+sudoku(Board,Vboard,Rboard,Cboard,B,Options) :-
+    constraints(Board,Vboard,Rboard,Cboard,Options),
     %search(Board),
-    search(Vboard).
+    ic:search(Vboard,0,first_fail,indomain,complete,[backtrack(B)]).
 
-constraints(Board,Vboard,Rboard,Cboard) :-
+constraints(Board,Vboard,Rboard,Cboard,Options) :-
   dim(Board,[9,9]), 
   dim(Vboard,[9,9]),
 	dim(Rboard,[9,9]),
@@ -29,34 +30,51 @@ constraints(Board,Vboard,Rboard,Cboard) :-
   Vboard :: [1..9],
 	Rboard :: [1..9],
 	Cboard :: [1..9],
+  (member(original,Options) -> 
+    channel(Board,Vboard),
+    constraints_original(Board)
+    ;
+    true
+  ),
+  (member(new,Options) ->
+    channel(Board,Vboard),
+    constraints_new(Vboard)
+    ;
+    true
+  ).
+  %channel(Vboard,Rboard,Cboard),
+	%constraints(Rboard,Cboard).
+	
+constraints_original(Board) :- %constraints for the original viewpoint
   (for(I,1,9), param(Board) do 
-    Row is Board[I,1..9], 
-    Col is Board[1..9,I], 
-    alldifferent(Row),
-    alldifferent(Col) 
-	), 
-  (multifor([J,K],1,7,3), param(Board) do 
-    Block is Board[J..J+2,K..K+2], 
-		flatten(Block, BlockList), 
-		alldifferent(BlockList) 
-	),
-  channel(Board,Vboard),
+      Row is Board[I,1..9], 
+      Col is Board[1..9,I], 
+      alldifferent(Row),
+      alldifferent(Col) 
+    ), 
+    (multifor([J,K],1,7,3), param(Board) do 
+      Block is Board[J..J+2,K..K+2], 
+      flatten(Block, BlockList), 
+      alldifferent(BlockList) 
+    ).
+
+%constraints for our new viewpoint
+constraints_new(Vboard) :-
   (for(I,1,9), param(Vboard) do 
     Row is Vboard[I,1..9], 
     Col is Vboard[1..9,I], 
     alldifferent(Row),
     alldifferent(Col) 
   ),
-  channel(Vboard,Rboard,Cboard),
-	constraints(Rboard,Cboard).
-	
+  constraints_square(Vboard). %different numbers in each square
+
 constraints(Rboard,Cboard) :-
 	constraints_rows(Rboard),
 	constraints_cols(Cboard).
 	
 constraints_rows(Rboard) :-
 	(for(I,1,9), param(Rboard) do
-		(for(J,1,9,3), param(Rboard,I) do
+		(for(J,1,7,3), param(Rboard,I) do
 			Block is Rboard[I,J..J+2],
 			Block::[J..J+2]
 		),
@@ -66,19 +84,35 @@ constraints_rows(Rboard) :-
 	
 constraints_cols(Cboard) :-
 	(for(I,1,9), param(Cboard) do
-		Row is Cboard[I,1..9],
-		alldifferent(Row),
-		(for(J,1,9,3), param(Cboard,I) do
+		Col is Cboard[I,1..9],
+		alldifferent(Col),
+		(for(J,1,7,3), param(Cboard,I) do
 			Cboard[I,J]::[1..3],
 			Cboard[I,J+1]::[4..6],
 			Cboard[I,J+2]::[7..9]
 		)
 	).
 
-search(Board) :-
-  term_variables(Board, Vars), 
-  labeling(Vars).
+constraints_square(Vboard) :-
+  (for(I,1,9), param(Vboard)
+  do
+    (for(J,1,7,3), param(Vboard,I)
+    do
+      V1 is Vboard[I,J],
+      V2 is Vboard[I,J+1],
+      V3 is Vboard[I,J+2],
+      #>=(V1,7,A),
+      #>=(V2,7,B),
+      #>=(V3,7,C),
+      A + B + C #= 1,
+      #=<(V1,3,D),
+      #=<(V2,3,E),
+      #=<(V3,3,F),
+      D + E + F #= 1
+    )
+  ).
 
+%channel between original viewpoint (Board) and our viewpoint (Vboard)
 channel(Board,Vboard) :-
   (multifor([A,B,K],1,9), param(Board,Vboard) do
     V is Board[A,B],
