5ef31cae769868c763fc0e25cf266a524f80e6fe
mastodon667 <herbert.gorissen@gmail.com>
2015-05-28 20:29:12 +0200

    Update bs_chr.pl

--- bs_chr.pl
+++ bs_chr.pl
@@ -1,82 +1,102 @@
-% author: Or Sharir <tnidtnid@GMAIL.COM>
-
 :-use_module(library(chr)).
 :-set_prolog_flag(chr_toplevel_show_store,false).
-:-chr_constraint cell/3, phase1/0, phase2/0,columns/1.
+:-chr_constraint cell/3,hint/0,guess/0,row/2,col/2,boat/1.
 
-phase1, columns([]) <=> true.
-phase1, cell(R,0,num(1)), columns(L) <=> select(C,L,L1) | cell(R,C,num(1)), columns(L1).
-cell(R,C,num(1)) \ cell(R,C,pos(_)) <=> true.
-cell(R1,C1,num(1)), cell(R2,C2,num(1)) <=> R1 = R2, C1 = C2 | fail.
-cell(R1,C1,num(1)), cell(R2,C2,num(1)) <=> R1 = R2+1, C1 = C2+1 | fail.
-cell(R1,C1,num(1)), cell(R2,C2,num(1)) <=> R1 = R2+1, C1 = C2-1 | fail.
-cell(R1,C1,num(1)), cell(R2,C2,num(1)) <=> R1 = R2-1, C1 = C2+1 | fail.
-cell(R1,C1,num(1)), cell(R2,C2,num(1)) <=> R1 = R2-1, C1 = C2-1 | fail.
-phase2, cell(R,C,pos(_)) <=> cell(R,C,num(0)).
 
-loop_phase1 :- find_chr_constraint(columns(_)), phase1, loop_phase1.
-loop_phase1 :- \+find_chr_constraint(columns(_)), loop_phase2.
+%hints
+cell(Row,Col,num(0))\cell(Row,Col,pos(_)),hint<=>true.
+cell(Row,Col,num(_))\cell(Row,Col,pos(_)),row(Row,[1|R]),col(Col,[1|C]),hint<=>row(Row,R),col(Col,C).
 
-loop_phase2 :- find_chr_constraint(cell(_,_,pos(_))), phase2, loop_phase2.
-loop_phase2 :- \+find_chr_constraint(cell(_,_,pos(_))), !.
+%propagation
+row(Row,[])\cell(Row,Col,pos(_))<=>cell(Row,Col,num(0)).
+col(Col,[])\cell(Row,Col,pos(_))<=>cell(Row,Col,num(0)).
+cell(Row,Col,num(Num))\cell(Row+1,Col+1,pos(_))<=>Num>0|cell(Row+1,Col+1,num(0)).
+cell(Row,Col,num(Num))\cell(Row-1,Col+1,pos(_))<=>Num>0|cell(Row-1,Col+1,num(0)).
+cell(Row,Col,num(Num))\cell(Row+1,Col-1,pos(_))<=>Num>0|cell(Row+1,Col-1,num(0)).
+cell(Row,Col,num(Num))\cell(Row-1,Col-1,pos(_))<=>Num>0|cell(Row-1,Col-1,num(0)).
+cell(Row,Col,num(Num))\cell(Row+1,Col,pos(_))<=>Num>0|cell(Row+1,Col,pos([0,Num])).
+cell(Row,Col,num(Num))\cell(Row-1,Col,pos(_))<=>Num>0|cell(Row-1,Col,pos([0,Num])).
+cell(Row,Col,num(Num))\cell(Row,Col+1,pos(_))<=>Num>0|cell(Row,Col+1,pos([0,Num])).
+cell(Row,Col,num(Num))\cell(Row,Col-1,pos(_))<=>Num>0|cell(Row,Col-1,pos([0,Num])).
+
+%guess
+cell(Row,Col,pos(Pos))\guess<=>member(N,Pos),cell(Row,Col,num(N)),hint.
+
+
+
+
 
 solve:-
-	problem(1,_,R,C),
+	problem(1,H,R,C),
 	length(R,N),
+	occurences(R,C,1),
 	N1 is N*N,
-	occurences(R,C,1,[],Rows,[],Cols),
-	columns(Cols),
-	make_coordinates(Rows),
 	create_cells(0,N,N1),
-	loop_phase1,
-	print_board(0,N1).
+	%make_boats,
+	process_hints(H),
+	loop,
+	print_board(0,N1,N).
 
-occurences([],[],_,R,R,C,C) :-!.
-occurences([Row|R],[Col|C],N,Tr,Rows,Tc,Cols) :-
+loop:- find_chr_constraint(cell(_,_,pos(_))),guess,loop.
+loop:- \+find_chr_constraint(cell(_,_,pos(_))),!.
+
+occurences([],[],_).
+occurences([Row|R],[Col|C],N) :-
 	N1 is N+1,
-	make_line(Row,[],Tr1,N),
-	make_line(Col,[],Tc1,N),
-	append(Tr,Tr1,Tr2),
-	append(Tc,Tc1,Tc2),
-	occurences(R,C,N1,Tr2,Rows,Tc2,Cols).
+	make_list(Row,[],R1),
+	make_list(Col,[],C1),
+	row(N,R1),
+	col(N,C1),
+	occurences(R,C,N1).
 	
-make_line(0,F,F,_).
-make_line(T,R,F,I) :-
+make_list(0,R,R).
+make_list(T,L,R) :-
 	T > 0,
 	T1 is T - 1,
-	make_line(T1,[I|R],F,I).
+	make_list(T1,[1|L],R).
 	
-make_coordinates([]).
-make_coordinates([Row|R]) :-
-	cell(Row,0,num(1)),
-	make_coordinates(R).
+make_boats :-
+	make_boat(1,5),
+	make_boat(2,6),
+	make_boat(3,6),
+	make_boat(4,4).
 	
-unique(_,_,[]).
-unique(R,C,[(R1,C1)|L]) :-
-	(
-		R \= R1
+make_boat(_,0).
+make_boat(B,T) :-
+	T > 0,
+	T1 is T - 1,
+	boat(B),
+	make_boat(B,T1).
+
+create_cells(S,_,S):-!.
+create_cells(P,N,S):-
+	NewP is P+1,
+	create_cells(NewP,N,S),
+	Row is P//N + 1,
+	Col is P mod N + 1,
+	cell(Row,Col,pos([0,1,2,3,4])).
+	
+process_hints([]) :- !.
+process_hints([(R,C,V)|H]) :-
+	process_hints(H),
+	cell(R,C,num(V)),hint.
+	
+
+%prints the board
+print_board(_,0,_). 
+print_board(P,T,N):-
+	Row is P//N + 1,
+	Col is P mod N + 1,
+	( find_chr_constraint(cell(Row,Col,num(_))) ->
+		find_chr_constraint(cell(Row,Col,num(V)))
 	;
-		C \= C1
+		find_chr_constraint(cell(Row,Col,pos([V|_])))
 	),
-	unique(R,C,L).
-
-create_cells(N,_,N):-!.
-create_cells(P,S,N):-
+	write(V),write(' '),
+	(Col == N -> nl;true),
 	NewP is P+1,
-	create_cells(NewP,S,N),
-	Row is P//S + 1,
-	Col is P mod S + 1,
-	cell(Row,Col,pos([0,1])).
-
-print_board(N,N).
-print_board(P,N):-
-	Row is P//10 + 1,
-	Col is P mod 10 + 1,
-	find_chr_constraint(cell(Row,Col,num(N))),
-	write(N),write(' '),
-	(Col == 10 -> nl;true),
-	NewP is P+1,
-	print_board(NewP).
+	NewT is T-1,
+	print_board(NewP,NewT,N).
 
 %example board:
 problem(1, [ (7,10,1), (1,6,0) ],
